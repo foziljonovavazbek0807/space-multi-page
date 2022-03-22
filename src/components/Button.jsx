db1b094e765b6ce6e55ef3da78c8f61854ec572c
Avazbek <foziljonovavazbek0807@gmail.com>
2022-03-22 23:04:34 +0500

    ok

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./Button.css";
+import React from 'react';
+import './Button.css';
 
-const STYLES = ["btn--primary", "btn--outline"];
+const STYLES = ['btn--primary', 'btn--outline'];
 
-const SIZES = ["btn--medium", "btn--large", "bnt--mobile", "btn--wide"];
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide'];
 
-const COLOR = ["primary", "blue", "red", "green"];
+const COLOR = ['primary', 'blue', 'red', 'green'];
 
 export const Button = ({
   children,
@@ -13,21 +13,19 @@ export const Button = ({
   onClick,
   buttonStyle,
   buttonSize,
-  buttonColor,
+  buttonColor
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
 
-  const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  const checkButtonColor = STYLES.includes(buttonColor)
-    ? buttonColor
-    : COLOR[0];
+  const checkButtonColor = COLOR.includes(buttonColor) ? buttonColor : null;
 
   return (
     <button
-      className={`btn $ {checkButtonStyle} $ {checkButtonSize} $ {checkButtonColor}`}
+      className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}
       onClick={onClick}
       type={type}
     >
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { MdFingerprint } from 'react-icons/md';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import {Button} from './Button';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { MdFingerprint } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -13,47 +13,53 @@ function Navbar() {
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth<= 960){
+    if (window.innerWidth <= 960) {
       setButton(false);
     } else {
       setButton(true);
     }
-  }
+  };
+
+  useEffect (() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
 
   return (
-    <div className='Navbar'>
-      <div className='navbar-container container'>
-        <Link to='/' className='navbar-logo'>
-          <MdFingerprint className='navbar-icon' />
+    <div className="Navbar">
+      <div className="navbar-container container">
+        <Link to="/" className="navbar-logo">
+          <MdFingerprint className="navbar-icon" />
           Space
         </Link>
-        <div className='menu-icon' onClick={handleClick}>
+        <div className="menu-icon" onClick={handleClick}>
           {click ? <FaTimes /> : <FaBars />}
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li className='nav-item'>
-            <Link to='/' className='nav-links'>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links">
               Home
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='/services' className='nav-links'>
+          <li className="nav-item">
+            <Link to="/services" className="nav-links">
               Services
             </Link>
           </li>
-          <li className='nav-item'>
-            <Link to='/products' className='nav-links'>
+          <li className="nav-item">
+            <Link to="/products" className="nav-links">
               Products
             </Link>
           </li>
-          <li className='nav-btn'>
+          <li className="nav-btn">
             {button ? (
-              <Link to='/sign-up' className='btn-link'>
-                <Button buttonStyle='btn--outline'>SIGN UP</Button>
+              <Link to="/sign-up" className="btn-link">
+                <Button buttonStyle="btn--outline">SIGN UP</Button>
               </Link>
             ) : (
-              <Link to='/sign-up' className='btn-link'>
-                <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>
+              <Link to="/sign-up" className="btn-link">
+                <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
                   SIGN UP
                 </Button>
               </Link>
